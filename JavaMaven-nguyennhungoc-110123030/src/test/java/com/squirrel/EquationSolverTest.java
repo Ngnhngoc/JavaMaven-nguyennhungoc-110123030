78a576ae5acf61be4465155f761437fcce64d2fa
Ngnhngoc <ngocsummer0309@gmail.com>
2026-05-28 07:44:43 +0000

    Them log hien thi chi tiet ten 3 ham test

--- JavaMaven-nguyennhungoc-110123030/src/test/java/com/squirrel/EquationSolverTest.java
+++ JavaMaven-nguyennhungoc-110123030/src/test/java/com/squirrel/EquationSolverTest.java
@@ -7,16 +7,19 @@ public class EquationSolverTest {
 
     @Test
     public void testVoSoNghiem() {
+        System.out.println("-> Dang chay Kiem thu: Truong hop VO SO NGHIEM");
         assertEquals("VSN", solver.giaiPhuongTrinhBac1(0, 0));
     }
 
     @Test
     public void testVoNghiem() {
+        System.out.println("-> Dang chay Kiem thu: Truong hop VO NGHIEM");
         assertEquals("VN", solver.giaiPhuongTrinhBac1(0, 5));
     }
 
     @Test
     public void testCoNghiem() {
+        System.out.println("-> Dang chay Kiem thu: Truong hop CO NGHIEM");
         assertEquals("CN", solver.giaiPhuongTrinhBac1(2, -4));
     }
 }
