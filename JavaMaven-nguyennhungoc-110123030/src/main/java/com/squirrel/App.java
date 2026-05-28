e877f496878ad716ac16a7efe3402e4ca4060b7b
Ngnhngoc <ngocsummer0309@gmail.com>
2026-05-28 07:47:23 +0000

    Them ham main va cau hinh in dap an ra log

--- JavaMaven-nguyennhungoc-110123030/src/main/java/com/squirrel/App.java
+++ JavaMaven-nguyennhungoc-110123030/src/main/java/com/squirrel/App.java
@@ -1,13 +1,17 @@
 package com.squirrel;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        EquationSolver solver = new EquationSolver();
+        
+        System.out.println("=======================================");
+        System.out.println("  KET QUA BIEN LUAN PHUONG TRINH BAC 1 ");
+        System.out.println("=======================================");
+        
+        System.out.println("Truong hop a = 0, b = 0  -> Dap an: " + solver.giaiPhuongTrinhBac1(0, 0));
+        System.out.println("Truong hop a = 0, b = 5  -> Dap an: " + solver.giaiPhuongTrinhBac1(0, 5));
+        System.out.println("Truong hop a = 2, b = -4 -> Dap an: " + solver.giaiPhuongTrinhBac1(2, -4));
+        
+        System.out.println("=======================================");
     }
 }
